72f50395a3cdda2f5fec0105624bf62b4dc4018a
genas2 <Gennas2@gmail.com>
2017-04-05 00:26:04 +0200

    Managed to get some data out of argv.

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv) {
-    printf("%s: %d\n", "test", argc);
+int main(int argc, char **argv) {
+    for (int i=0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
 }
 
